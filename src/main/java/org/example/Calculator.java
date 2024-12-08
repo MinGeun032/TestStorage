36192be0cce05ab11681014eef78818be45c0bdf
MinGeun032 <a8159031@gmail.com>
2024-12-08 23:17:49 +0900

    new test

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        System.out.println("Test");
+        System.out.println("One More Test");
+    }
+}
